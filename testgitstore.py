54cd5f186c380aa0d696cae5803fa53f490b3925
Sam Jones <sam@jonez.info>
2017-10-05 14:42:18 +0100

    Added a passing test for file listing.

--- testgitstore.py
+++ testgitstore.py
@@ -26,6 +26,16 @@ class TestGitStore(unittest.TestCase):
 		self.gitstore.add_file("/foobar/directory/testfile.json",data,author,"testing")
 		self.assertTrue(True)
 
+	def test_list_files(self):
+		author = self.gitstore.author('Bob Carmack','bob@example.org')
+		data = '{"id":42,"testString":"the quick brown fox"}'
+		self.gitstore.add_file("/testfile.json",data,author,"testing")
+		self.gitstore.add_file("/testfiletwo.json",data,author,"testing")
+		files = self.gitstore.list_files("/")
+		self.assertTrue(len(files) == 2)
+		self.assertEqual("testfile.json",files[0])
+		self.assertEqual("testfiletwo.json",files[1])
+
 
 if __name__ == '__main__':
     unittest.main()
