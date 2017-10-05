82bdeec5cba83fb1a306aa0199ea9b4623efb1bc
Sam Jones <sam@jonez.info>
2017-10-05 14:39:21 +0100

    Added a passing test for adding a directory with file to the store.

--- testgitstore.py
+++ testgitstore.py
@@ -20,6 +20,11 @@ class TestGitStore(unittest.TestCase):
 		self.gitstore.add_file("/testfile.json",data,author,"testing")
 		self.assertTrue(True)
 
+	def test_add_directory(self):
+		author = self.gitstore.author('Bob Carmack','bob@example.org')
+		data = '{"id":42,"testString":"the quick brown fox"}'
+		self.gitstore.add_file("/foobar/directory/testfile.json",data,author,"testing")
+		self.assertTrue(True)
 
 
 if __name__ == '__main__':
